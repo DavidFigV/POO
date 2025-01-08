8d7490976d03044053a460f617327d6d1c945989
DavidFigV <tutroyano2001@gmail.com>
2025-01-08 08:50:43 -0600

    Creando atributos de la Clase Personaje

--- poo_con_python.py
+++ poo_con_python.py
@@ -1,2 +1,22 @@
-hola_estoy_escribiendo_desde_snake_case
-holaEstoyEscribiendoDesdeCamelCase
+class Personaje:
+    #Atributos de la clase
+    nombre = "Player"
+    fuerza = 0
+    inteligencia = 0
+    defensa = 0
+    vida = 0
+    
+#Variable de constructor vacío de la clase
+mi_personaje = Personaje()
+#Modificando los valores de los atributos
+mi_personaje.nombre = "Damaris"
+mi_personaje.fuerza = 1
+mi_personaje.inteligencia = 5
+mi_personaje.defensa = 10
+mi_personaje.vida = 3
+print("El nombre del personaje es",mi_personaje.nombre)
+print("La fuerza del personaje es",mi_personaje.fuerza)
+print("La inteligencia del personaje es",mi_personaje.inteligencia)
+print("La defensa del personaje es",mi_personaje.defensa)
+print("La vida del personaje es",mi_personaje.vida)
+        
