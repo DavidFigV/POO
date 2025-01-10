dbcee41d96cdc2b58bc0a0da6aa16399a50125a4
DavidFigV <tutroyano2001@gmail.com>
2025-01-10 08:57:53 -0600

    creando metodos de personaje

--- poo_con_python.py
+++ poo_con_python.py
@@ -6,17 +6,46 @@ class Personaje:
     defensa = 0
     vida = 0
     
-#Variable de constructor vacío de la clase
-mi_personaje = Personaje()
-#Modificando los valores de los atributos
-mi_personaje.nombre = "Damaris"
-mi_personaje.fuerza = 1
-mi_personaje.inteligencia = 5
-mi_personaje.defensa = 10
-mi_personaje.vida = 3
-print("El nombre del personaje es",mi_personaje.nombre)
-print("La fuerza del personaje es",mi_personaje.fuerza)
-print("La inteligencia del personaje es",mi_personaje.inteligencia)
-print("La defensa del personaje es",mi_personaje.defensa)
-print("La vida del personaje es",mi_personaje.vida)
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
+    
+    def atributos(self):
+        print(self.nombre)
+        print("- Fuerza: ",self.fuerza)
+        print("- Inteligencia: ",self.inteligencia)
+        print("- Defensa: ",self.defensa)
+        print("- Vida: ",self.vida)
         
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza += fuerza
+        self.inteligencia += inteligencia
+        self.defensa += defensa
+        
+    def esta_vivo(self):
+        return self.vida > 0
+    
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+        
+    def daño(self, enemigo):
+        return self.fuerza - enemigo.defensa
+    
+    def atacar(self, enemigo):
+        daño = self.daño(enemigo)
+        enemigo.vida -= daño
+        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        if not enemigo.esta_vivo():
+            enemigo.morir()
+        print("Vida de", enemigo.nombre, "es", enemigo.vida)
+        
+#Variable de constructor de la clase
+mi_personaje = Personaje("El Troyano", 200, 15, 12, 20)
+mi_enemigo = Personaje("Dario", 10, 15, 12, 20)
+mi_personaje.atributos()
+mi_personaje.atacar(mi_enemigo)
+mi_enemigo.atributos()
